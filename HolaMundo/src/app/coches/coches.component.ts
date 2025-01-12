4bdb714f95425a866ebdcdf32aa2d6fae7c51d80
Isra Trabajo <israelfargasasquith@gmail.com>
2025-01-12 22:40:19 +0100

    Froms

--- HolaMundo/src/app/coches/coches.component.ts
+++ HolaMundo/src/app/coches/coches.component.ts
@@ -9,13 +9,18 @@ import { Coche } from "./coches";
 export class CochesComponent {
 
     public coche: Coche;
+    public coches: Array<Coche>;
 
     constructor() {
-        this.coche = new Coche("Toyota Corola", "1000", "Black");
+        this.coche = new Coche("", "", "");
+        this.coches = [];
+        this.coches.push(new Coche("Toyota Corola", "1000", "Black"));
+        this.coches.push(new Coche("Seat Panda", "420", "Pink"));
     }
 
     onSubmmit() {
         console.log(this.coche);
+        this.coches.push(this.coche);
     }
 
 }

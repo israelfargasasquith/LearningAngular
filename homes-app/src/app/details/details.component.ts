4a4bcabdb17ebc7d41938834ab1a90d572f645c3
Isra Trabajo <israelfargasasquith@gmail.com>
2025-03-23 11:09:00 +0100

    Adding routing

---
+++ homes-app/src/app/details/details.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details works! {{housingLocationID}}
+    </p>
+  `,
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationID = 0;
+  constructor() {
+    this.housingLocationID = Number(this.route.snapshot.params['id'])
+  }
+}
